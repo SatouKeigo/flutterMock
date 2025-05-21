573b4d895c80ffbc13273e86572e662d2a95f16a
keigo <kei159779@gmail.com>
2025-05-21 09:41:00 +0900

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-// main.dart
 import 'package:flutter/material.dart';
-import 'FirstPage.dart';
-import 'SecondPage.dart';
-import 'ThirdPage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -12,115 +10,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Multi-Page App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
+      title: '天気アプリ',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: WeatherHomePage(),
     );
   }
 }
 
-// HomePage.dart
-class HomePage extends StatelessWidget {
+class WeatherHomePage extends StatefulWidget {
+  @override
+  _WeatherHomePageState createState() => _WeatherHomePageState();
+}
+
+class _WeatherHomePageState extends State<WeatherHomePage> {
+  final TextEditingController _controller = TextEditingController();
+  String? _temperature;
+  String? _description;
+  String? _error;
+
+  Future<void> fetchWeather(String city) async {
+    final apiKey =
+        '5c0eb5159153499d04404e4d370b33fd'; // ← OpenWeatherMapのAPIキーを入れてね
+    final url =
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric&lang=ja';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          _temperature = '${data['main']['temp']}℃';
+          _description = data['weather'][0]['description'];
+          _error = null;
+        });
+      } else {
+        setState(() {
+          _error = '天気情報を取得できませんでした（${response.statusCode}）';
+          _temperature = null;
+          _description = null;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _error = '通信エラーが発生しました';
+        _temperature = null;
+        _description = null;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      body: Center(
+      appBar: AppBar(title: Text('天気アプリ')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FirstPage()),
-                );
-              },
-              child: Text('Go to First Page'),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(labelText: '都市名を入力してください'),
             ),
+            SizedBox(height: 10),
             ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondPage()),
-                );
-              },
-              child: Text('Go to Second Page'),
+              onPressed: () => fetchWeather(_controller.text),
+              child: Text('天気を取得'),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ThirdPage()),
-                );
-              },
-              child: Text('Go to Third Page'),
-            ),
+            SizedBox(height: 20),
+            if (_temperature != null && _description != null) ...[
+              Text('気温：$_temperature', style: TextStyle(fontSize: 20)),
+              Text('天気：$_description', style: TextStyle(fontSize: 20)),
+            ],
+            if (_error != null) ...[
+              SizedBox(height: 20),
+              Text(_error!, style: TextStyle(color: Colors.red)),
+            ],
           ],
-        ),
-      ),
-    );
-  }
-}
-
-// FirstPage.dart
-class FirstPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Back to Home Page'),
-        ),
-      ),
-    );
-  }
-}
-
-// SecondPage.dart
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Back to Home Page'),
-        ),
-      ),
-    );
-  }
-}
-
-// ThirdPage.dart
-class ThirdPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Third Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Back to Home Page'),
         ),
       ),
     );
