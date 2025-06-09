a8bee929aae16b5b43bac84bdd21ee7facbba54e
keigo <kei159779@gmail.com>
2025-06-09 13:29:33 +0900

    デバッグの帯を消した

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '天気アプリ',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: WeatherHomePage(),
